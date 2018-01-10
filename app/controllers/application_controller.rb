8465e709606359153d14fa33aec9bb318dfc15e5
Anoob Bava <anoob.bava@gmail.com>
2018-01-11 00:43:24 +0530

    Login Using email or user_name

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
+# 
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :allow_parameters, if: :devise_controller?
+
+  protected
+
+  def allow_parameters
+    new_attributes = [:user_name, :email, :first_name, :last_name, :password,
+                      :password_confirmation, :remember_me]
+    devise_parameter_sanitizer.permit :sign_up, keys: new_attributes
+    devise_parameter_sanitizer.permit :account_update, keys: new_attributes
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,18 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, authentication_keys: [:login]
+  attr_accessor :login
+
+  validates :user_name, presence: :true, uniqueness: { case_sensitive: false }
+validates_format_of :user_name, with: /^[a-zA-Z0-9_\.]*$/, :multiline => true
+
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions.to_hash).where(["lower(user_name) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    elsif conditions.has_key?(:user_name) || conditions.has_key?(:email)
+      where(conditions.to_hash).first
+    end
+  end
 end
