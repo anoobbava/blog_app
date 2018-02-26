354dc54e65b04a6108c4238d854df46f4bac6461
Anoob Bava <anoob.bava@gmail.com>
2018-02-26 20:54:20 +0530

    Comment-created via AJAX

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,27 @@
+# encoding: utf-8
+
+class CommentsController < ApplicationController
+  def create
+    @article = Article.find(params[:article_id])
+    comment = Comment.new(comment_params)
+    if comment.save
+      flash.now[:success] = 'commented successfully'
+      @comments = @article.comments
+    else
+      flash[:notice] = "Error on comment Creation >> #{comment.errors.full_messages.to_sentence}"
+    end
+    render layout: false
+  end
+
+  def destroy
+    comment = Comment.find(params[:id])
+    redirect_to(comment.article) if comment.destroy
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:description).merge(user_id: current_user.id,
+                                                        article_id: params[:article_id])
+  end
+end
