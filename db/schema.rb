286a17233ad838f58f008d1325ea6f1fcc93cbb4
Anoob Bava <anoob.bava@gmail.com>
2018-03-08 00:21:54 +0530

    Carrierwave for User profile

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180306145520) do
+ActiveRecord::Schema.define(version: 20180307180739) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -68,6 +68,7 @@ ActiveRecord::Schema.define(version: 20180306145520) do
     t.string "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
+    t.string "image"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
